aa509d518182547cef8848d959c8f4a054265fa3
PhilDL <philippe.lattention@hotmail.fr>
2021-10-14 08:57:16 +0400

    [MIG] Rewrite the main View Component

--- static/src/owl_tree_view/owl_tree_view.js
+++ static/src/owl_tree_view/owl_tree_view.js
@@ -1,42 +1,92 @@
-odoo.define("owl_tutorial_views.OWLTreeView", function (require) {
-  "use strict";
+/** @odoo-module **/
+import { registry } from "@web/core/registry";
+import { XMLParser } from "@web/core/utils/xml";
+import { useModel } from "@web/views/helpers/model";
+import { standardViewProps } from "@web/views/helpers/standard_view_props";
+import { Layout } from "@web/views/layout";
+import { OWLTreeModel } from "@owl_tutorial_views/owl_tree_view/owl_tree_model";
+import { OWLTreeRenderer } from "owl_tutorial_views.OWLTreeRenderer";
 
-  const OWLTreeController = require("owl_tutorial_views.OWLTreeController");
-  const OWLTreeModel = require("owl_tutorial_views.OWLTreeModel");
-  const { OWLTreeRenderer } = require("owl_tutorial_views.OWLTreeRenderer");
-  const AbstractView = require("web.AbstractView");
-  const core = require("web.core");
-  const RendererWrapper = require("web.RendererWrapper");
-  const view_registry = require("web.view_registry");
+class OWLTreeView extends owl.Component {
+  /**
+   * Parse the props.arch Root node and transform the attributes into a JS object
+   * the will correspond to the options
+   *
+   * @private
+   * @returns {Object} Options
+   **/
+  _parseArchOptions() {
+    const parser = new XMLParser();
+    const arch = parser.parseXML(this.props.arch);
+    const options = {};
+    for (const attr of arch.getAttributeNames()) {
+      options[attr] = arch.getAttribute(attr);
+    }
+    return options;
+  }
 
-  const _lt = core._lt;
+  /**
+   * Standard setup function of OWL Component, here we parse the XML
+   * template to get the options and instantiate the Model.
+   **/
+  setup() {
+    this.archOptions = this._parseArchOptions();
+    this.model = useModel(OWLTreeModel, {
+      resModel: this.props.resModel,
+      domain: this.props.domain,
+    });
+  }
 
-  const OWLTreeView = AbstractView.extend({
-    accesskey: "m",
-    display_name: _lt("OWLTreeView"),
-    icon: "fa-indent",
-    config: _.extend({}, AbstractView.prototype.config, {
-      Controller: OWLTreeController,
-      Model: OWLTreeModel,
-      Renderer: OWLTreeRenderer,
-    }),
-    viewType: "owl_tree",
-    searchMenuTypes: ["filter", "favorite"],
+  /**
+   * @param {Object} ev
+   * @param {Object} ev.data contains the payload
+   */
+  async _onTreeItemClicked(ev) {
+    ev.stopPropagation();
+    if (ev.detail.data.children === undefined) {
+      await this.model.expandChildrenOf(
+        ev.detail.data.id,
+        ev.detail.data.parent_path
+      );
+    } else {
+      this.model.toggleChildrenVisibleForItem(ev.detail.data);
+    }
+  }
 
-    /**
-     * @override
-     */
-    init: function () {
-      this._super.apply(this, arguments);
-    },
+  /**
+   * @param {Object} ev
+   * @param {Object} ev.data.itemMoved is the item that has been dragged
+   * @param {Object} ev.data.newParent is the new target parent of the dropped item
+   */
+  async _onChangeItemTree(ev) {
+    ev.stopPropagation();
+    let itemMoved = ev.detail.itemMoved;
+    let newParent = ev.detail.newParent;
+    await this.model.changeParent(itemMoved.id, newParent.id);
 
-    getRenderer(parent, state) {
-      state = Object.assign(state || {}, this.rendererParams);
-      return new RendererWrapper(parent, this.config.Renderer, state);
-    },
-  });
+    // Refresh old parent
+    let oldParent = await this.model.refreshNode(itemMoved.parent_id[0]);
+    await this.model.expandChildrenOf(oldParent.id, oldParent.parent_path);
 
-  view_registry.add("owl_tree", OWLTreeView);
+    // Refresh new parent
+    await this.model.expandChildrenOf(
+      ev.detail.newParent.id,
+      ev.detail.newParent.parent_path
+    );
+  }
+}
+OWLTreeView.type = "owl_tree";
+OWLTreeView.display_name = "OWLTreeView";
+OWLTreeView.icon = "fa-indent";
+OWLTreeView.multiRecord = true;
+OWLTreeView.searchMenuTypes = ["filter", "favorite"];
+OWLTreeView.components = { Layout, OWLTreeRenderer };
+OWLTreeView.props = {
+  ...standardViewProps,
+};
+OWLTreeView.template = owl.tags.xml/* xml */ `
+<Layout viewType="'owl_tree'">
+    <OWLTreeRenderer archOptions="archOptions" model="model" t-on-tree_item_clicked="_onTreeItemClicked" t-on-change_item_tree="_onChangeItemTree"/>
+</Layout>`;
 
-  return OWLTreeView;
-});
+registry.category("views").add("owl_tree", OWLTreeView);
