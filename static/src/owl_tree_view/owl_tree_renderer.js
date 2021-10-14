36959142d6f31b792fa358c67c4b2f64f75787d7
PhilDL <philippe.lattention@hotmail.fr>
2021-10-14 09:05:03 +0400

    [MIG] Update the Renderer to accept new props

--- static/src/owl_tree_view/owl_tree_renderer.js
+++ static/src/owl_tree_view/owl_tree_renderer.js
@@ -1,21 +1,17 @@
 /** @odoo-module alias=owl_tutorial_views.OWLTreeRenderer default=0 **/
 const { useState } = owl.hooks;
 import AbstractRendererOwl from "web.AbstractRendererOwl";
-import QWeb from "web.QWeb";
-import session from "web.session";
 import { TreeItem } from "@owl_tutorial_views/components/tree_item/TreeItem";
 
 export class OWLTreeRenderer extends AbstractRendererOwl {
   constructor(parent, props) {
     super(...arguments);
-    this.qweb = new QWeb(this.env.isDebug(), { _s: session.origin });
     this.state = useState({
-      localItems: props.items || [],
       countField: "",
     });
-    if (this.props.arch.attrs.count_field) {
+    if (this.props.archOptions.count_field) {
       Object.assign(this.state, {
-        countField: this.props.arch.attrs.count_field,
+        countField: this.props.archOptions.count_field,
       });
     }
   }
@@ -25,23 +21,15 @@ const components = { TreeItem };
 Object.assign(OWLTreeRenderer, {
   components,
   defaultProps: {
-    items: [],
+    archOptions: {},
   },
   props: {
-    arch: {
+    archOptions: {
       type: Object,
       optional: true,
     },
-    items: {
-      type: Array,
-    },
-    isEmbedded: {
-      type: Boolean,
-      optional: true,
-    },
-    noContentHelp: {
-      type: String,
-      optional: true,
+    model: {
+      type: Object,
     },
   },
   template: "owl_tutorial_views.OWLTreeRenderer",
